526851b9dfc849e9ddef7b91f3f59086e69d5b59
Gijs-Hoeben <gijs.hoeben@outlook.com>
2019-11-26 09:20:16 +0100

    praktijk toets versiebeheer

---
+++ ArrayPrinter.java
@@ -0,0 +1,11 @@
+public class ArrayPrinter {
+
+	public static void main(String[] args) {
+		ArrayPrinter printer = new ArrayPrinter();
+		printer.printarray();
+	}
+	
+	private void printarray() {
+		int[] getal = new int[5];
+	}
+}
